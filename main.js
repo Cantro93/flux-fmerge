f780018e4579c757fd6e0f77ac8bcb468f496477
Cantro93 <antonsolt48@gmail.com>
2023-05-12 09:28:24 +0200

    Fixed export bugs and added GUI message
    Fixed handling of export commands.
    Added completion message for GUI.

--- main.js
+++ main.js
@@ -1,31 +1,46 @@
-const {app} = require('electron');
+const {app, dialog} = require('electron');
 const fs = require('fs');
 const path = require('path');
 function merge(file) {
 	let fl = fs.readFileSync(file).toString();
 	let mergers = [...fl.matchAll(/(\$merge\(".*"\);)/g)];
 	let q = '';
-	let targarr, tvarr = [];
+	let targarr = [], tvarr = [], fp = [];
+	targarr = fl.match(/(\$target\(".*"\);)/g);
 	if (mergers.length > 0) {
-		targarr = [...fl.match(/(\$target\(".*"\);)/g)];
 		if (mergers[0].length > 0) {
 		for (let merger of mergers[0]) {
 			q = merger.replace(/(\$merge\(")/g, '').replace(/("\);)/g, '');
-			fl = fl.replaceAll(merger, merge(path.join(path.dirname(file), q)));
+			fl = fl.replaceAll(merger, merge(path.join(path.dirname(file), q)).content);
 		}
 		}
+	};
+	if (targarr != null) {
 		fl = fl.replace(/(\$target\(".*"\);)/g, '').replace(/(\r\n){3,}/g, '\r\n');
 		for (let targ of targarr) {
 			let tval = targ.replace(/(\$target\(")/g, '').replace(/("\);)/g, '');
 			tvarr[tvarr.length] = tval;
+			fp[fp.length] = path.join(path.dirname(file), tval);
 		}
-		for (let tval of tvarr) fs.writeFileSync(path.join(path.dirname(file), tval), fl);
-	};
-	return fl;
+		for (let tval of fp) fs.writeFileSync(tval, fl);
+		return {content: fl, targets: fp};
+	}
+	else return {content: fl};
 }
 app.whenReady().then(() => {
 	let a = ((process.argv[0] == 'electron') ? 0 : 1);
 	console.log(`Running HX Flux FMerge on the following file ${process.argv[1 + a]}`);
-	merge(process.argv[1 + a]);
-    	console.log('Work ended.');
+	let targets = merge(process.argv[1 + a]).targets;
+    console.log('Work ended');
+	let list = '';
+	for (let target of targets) {
+		list += (target + '\r\n');
+	}
+	dialog.showMessageBoxSync({
+		message: `File '${process.argv[1 + a]}'\r\nwas successfully generated and exported to following locations:\r\n${list}`,
+		type: 'none',
+		buttons: ['OK'],
+		title: 'HX Flux FMerge'
+	});
+	app.exit(0);
 });
